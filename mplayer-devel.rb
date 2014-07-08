0e8924609d4b9ff0c4b230182314355f9872fe40
Chris Jones <cjones@gmail.com>
2014-07-08 13:09:47 -0700

    check in work in progress on mplayer-devel formula

--- mplayer-devel.rb
+++ mplayer-devel.rb
@@ -1,31 +1,54 @@
 require 'formula'
 
 class BrokenFormula < Requirement
-  # i..think this is the right syntax. or one of the many, i guess. ruby = :(
   fatal true
-  satisfy { 4 < 2 }
-  def message
-    "This module is currently disabled due to extreme brokeness."
+  satisfy { ENV['BUILD_BROKEN_MPLAYER'] == 'yes' }
+  def message; "This formula is disabled (still a work in progress)" end
+end
+
+class NoExternalsSubversionDownloadStrategy < SubversionDownloadStrategy
+  def fetch_repo target, url, revision=nil, ignore_externals=false
+    super target, url, revision, true
   end
 end
 
 class MplayerDevel < Formula
   homepage 'http://www.mplayerhq.hu/'
-  head "svn://svn.mplayerhq.hu/mplayer/trunk", :using => :svn
+
+  version "36449"
+  url "svn://svn.mplayerhq.hu/mplayer/trunk",
+    :using => NoExternalsSubversionDownloadStrategy,
+    :revision => "#{version}"
+
+  #patch :DATA
 
   option 'without-osd', 'Disable on-screen display (menus, etc)'
   option 'without-encoders', 'Build without common encoders (x264, xvid, etc.)'
 
+  # hamstring formula before it can build deps
   depends_on BrokenFormula
 
+  # build dependencies
   depends_on 'git' => :build
   depends_on 'subversion' => :build
   depends_on 'pkg-config' => :build
   depends_on 'yasm' => :build
 
+  # compiler blacklsit stuff
+  depends_on "gcc" if MacOS.version >= :mountain_lion
+  fails_with :clang do
+    build 211
+    cause 'Inline asm errors during compile on 32bit Snow Leopard.'
+  end unless MacOS.prefer_64_bit?
+  
+  # these are in the dupes tap. list as :optional?
+  #depends_on 'libiconv'
+  #depends_on 'ncurses'
+  #depends_on 'zlib'
+ 
+  # run depends
   depends_on 'lame'
   depends_on 'libass'
-  depends_on 'libiconv'
   depends_on 'libjpeg'
   depends_on 'libmad'
   depends_on 'libogg'
@@ -33,11 +56,9 @@ class MplayerDevel < Formula
   depends_on 'libpng'
   depends_on 'libvorbis'
   depends_on 'lzo'
-  depends_on 'ncurses'
   depends_on 'openjpeg'
   depends_on 'opus'
   depends_on 'theora'
-  depends_on 'zlib'
 
   if build.with? 'osd'
     depends_on 'fontconfig'
@@ -53,30 +74,26 @@ class MplayerDevel < Formula
   end
 
   def install
-    odie "This formula is .. a work in progress. To put it mildly. DO NOT USE!"
-
     dvdnav_url = "svn://svn.mplayerhq.hu/dvdnav/trunk"
     dvdnav_ver = "1257"
+    ffmpeg_url = "git://git.videolan.org/ffmpeg.git"
     ffmpeg_ver = "09887096734eabaac5454b3fb5e4489457ac9434"
 
-    system "#{bin}/svn", "export", "-r#{dvdnav_ver}", "#{dvdnav_url}/libdvdnav/src", "./libdvdnav/"
-    system "#{bin}/svn", "export", "-r#{dvdnav_ver}", "#{dvdnav_url}/libdvdread/src", "./libdvdread4/"
-    system "#{bin}/git", "clone", "--depth", "5000", "git://git.videolan.org/ffmpeg.git", "./ffmpeg"
+    system "svn", "export", "--force", "-r#{dvdnav_ver}", "#{dvdnav_url}/libdvdnav/src", "./libdvdnav/"
+    system "svn", "export", "--force", "-r#{dvdnav_ver}", "#{dvdnav_url}/libdvdread/src", "./libdvdread4/"
+    system "git", "clone", "--depth", "5000", "#{ffmpeg_url}", "./ffmpeg"
 
     cd 'ffmpeg' do
-      system "git", "checkout", "-f" "#{ffmpeg_ver}"
+      system "git", "checkout", "-f", "#{ffmpeg_ver}"
     end
 
-    #--extra-cflags="#{ENV.cflags}"
-    #--extra-ldflags="#{ENV.ldflags}"
-    #--cc=#{ENV.cc}
-    #--host-cc=#{ENV.cc}
-    #--datadir="#{prefix}/share/mplayer-devel"
-    #--confdir="#{prefix}/etc/mplayer-devel"
-    #--mandir="#{prefix}/share/man"
+    ENV.O1 if ENV.compiler == :llvm
+    ENV.append_to_cflags "-mdynamic-no-pic" if Hardware.is_32_bit? && Hardware::CPU.intel? && ENV.compiler == :clang
 
     args = %W[
-      --prefix="#{prefix}"
+      --prefix=#{prefix}
+      --cc=#{ENV.cc}
+      --host-cc=#{ENV.cc}
       --enable-png
       --enable-jpeg
       --enable-liblzo
@@ -110,13 +127,20 @@ class MplayerDevel < Formula
       --disable-apple-remote
     ]
 
-    if build.with? 'osd'
-      args << "--enable-menu"
+    if MacOS.prefer_64_bit?
+      args << "--disable-qtx"
+      arch = "x86_64"
+    else
+      args << "--enable-qtx"
+      arch = "i386"
     end
 
-    if build.without? 'osd'
-      args << "--disable-fontconfig"
-      args << "--disable-freetype"
+    args << "--target=#{arch}-Darwin"
+
+    if build.with? 'osd'
+      args << "--enable-menu"
+    else
+      args << "--disable-fontconfig" << "--disable-freetype"
     end
 
     if build.without? 'encoders'
@@ -127,15 +151,10 @@ class MplayerDevel < Formula
       args << "--disable-twolame"
     end
 
-    odie "How did you get here? NO."
-    system "sh -c 'exit 1' -"
-    # system "./configure", *args
-    # system "make"
-    # system "make install"
-  end
-
-  test do
-    system "sh -c true"
+    system "./configure", *args
+    system "make"
+    odie "aborting before insstall phase"
+    system "make install"
   end
 end
 
